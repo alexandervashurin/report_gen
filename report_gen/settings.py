c432d6032d6bd6d11cca93ccb2d894757efda3bb
Александр Вашурин <alexandervashurin@yandex.ru>
2022-02-19 21:26:29 +0500

    fix

--- report_gen/settings.py
+++ report_gen/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'report_gen.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'report_psi',
+        'NAME': 'report_gendb',
         'USER': 'reportuser',
         "PASSWORD": 'bailando123',
     }
